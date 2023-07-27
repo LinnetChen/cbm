d96d519d15a8d33424b0b9795b751794aef2569a
ShaneLin <shane_lin@digeam.com>
2023-07-27 20:59:42 +0800

    laravel-adminv1.0

--- resources/views/test_launcher.blade.php
+++ resources/views/test_launcher.blade.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
-    <head>
-    </head>
-    <body>
-        <a href="https://cbo.digeam.com/MembershipTransfer" target="_blank"><img
-            src="/img/launcher.png"></a>
-    </body>
+
+<head>
+    <style>
+        body {
+            margin: 0px;
+        }
+    </style>
+</head>
+
+<body>
+    <a href="https://cbo.digeam.com/MembershipTransfer" target="_blank"><img src="/img/launcher.png"></a>
+</body>
+
 </html>
