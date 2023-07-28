94ea700e5fda5d756b72b530b65270dd078fb23a
ShaneLin <shane_lin@digeam.com>
2023-07-28 13:55:11 +0800

    prereg_backendv1.0

--- app/Models/transfer_user.php
+++ app/Models/transfer_user.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 class transfer_user extends Model
 {
     protected $table = 'transfer_user';
+    use DefaultDatetimeFormat;
 }
--- app/Models/try_login_log.php
+++ app/Models/try_login_log.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 class try_login_log extends Model
 {
     protected $table = 'try_login_log';
-
+    use DefaultDatetimeFormat;
 }
