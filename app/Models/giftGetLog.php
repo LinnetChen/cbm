1efbebba33ad7b78aec710381778ca7c62a894a3
ShaneLin <shane_lin@digeam.com>
2023-09-21 15:54:44 +0800

    20230921-v1.3

--- app/Models/giftGetLog.php
+++ app/Models/giftGetLog.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Database\Eloquent\Model;
 
 class giftGetLog extends Model
 {
     protected $table = 'gift_getlog';
+    use DefaultDatetimeFormat;
 }
